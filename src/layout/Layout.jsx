a2bf35ae18f52b940a9bfa4f018390d3c87f0281
enzocazenave <enzocazenavedev@hotmail.com>
2024-07-24 15:31:51 -0300

    Add boilerplate code

---
+++ src/layout/Layout.jsx
@@ -0,0 +1,12 @@
+import { Footer } from "./Footer";
+import { Navbar } from "./Navbar";
+
+export const Layout = () => {
+  return (
+    <>
+        <Navbar />
+      
+        <Footer />
+    </>
+  );
+};
