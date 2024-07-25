c585f6e5603cbbc869f77cda024e7aea97d5a503
Yamil Rios <gabriel.rios.93@gmail.com>
2024-07-25 08:37:49 -0300

    new page for create new product

--- src/pages/CreateProductPage.jsx
+++ src/pages/CreateProductPage.jsx
@@ -5,14 +5,15 @@ export const CreateProductPage = () => {
             <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
                 <div className="flex flex-col items-center">
                     <h1>Crear producto</h1>
-                    <Input type="text" title="Nombre"/>
-                    <Input type="text" tile="Stock"/>
-                    <Input type="text" tile="Precio"/>
-                    <Input type="text" tile="Descripción"/>
-                    <Input type="text" tile="Talle"/>
-                    <Input type="text" tile="URL"/>
-                    <select value="Selecciona una caterogia">
-                        <option value="Selecciona una caterogia" disabled>
+                    <Input type="text" label="Nombre"/>
+                    <Input type="text" label="Stock"/>
+                    <Input type="text" label="Precio"/>
+                    <Input type="text" label="Descripción"/>
+                    <Input type="text" label="Talle"/>
+                    <Input type="text" label="URL"/>
+                    <select value={0}>
+                        <option value={0} disabled>
+                            Selecciona una caterogia...
                         </option>
                     </select>
                     <button className="border">Crear</button>
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,26 +1,28 @@
-import { Routes, Route } from "react-router-dom";
+import {Routes, Route} from "react-router-dom";
 import {
-  HomePage,
-  LoginPage,
-  RegisterPage,
-  ProductsPage,
-  ProductPage,
-  CartPage,
-  OrderPage,
-  CheckoutPage
+    HomePage,
+    LoginPage,
+    RegisterPage,
+    ProductsPage,
+    ProductPage,
+    CartPage,
+    OrderPage,
+    CheckoutPage,
+    CreateProductPage
 } from "../pages";
 
 export const AppRouter = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/register" element={<RegisterPage />} />
-      <Route path="/products" element={<ProductsPage />} />
-      <Route path="/products/:id" element={<ProductPage />} />
-      <Route path="/cart" element={<CartPage />} />
-      <Route path="/cart/checkout" element={<CheckoutPage />} />
-      <Route path="/order/:id" element={<OrderPage />} />
-    </Routes>
-  );
+    return (
+        <Routes>
+            <Route path="/" element={<HomePage/>}/>
+            <Route path="/login" element={<LoginPage/>}/>
+            <Route path="/register" element={<RegisterPage/>}/>
+            <Route path="/products" element={<ProductsPage/>}/>
+            <Route path="/products/:id" element={<ProductPage/>}/>
+            <Route path="/products/new" element={<CreateProductPage/>}/>
+            <Route path="/cart" element={<CartPage/>}/>
+            <Route path="/cart/checkout" element={<CheckoutPage/>}/>
+            <Route path="/order/:id" element={<OrderPage/>}/>
+        </Routes>
+    );
 };
