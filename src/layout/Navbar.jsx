d3c1b29429a9f32d8a81da2f22462091d5a7f063
nihuerta <nicolasagustinhuerta@hotmail.com>
2024-07-26 19:13:35 -0300

    boolean sesión iniciada

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,27 +1,44 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export const Navbar = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(true);
+
+  const handleLogin = () => {
+    // Lógica para autenticar al usuario
+    setIsAuthenticated(true);
+  };
+
+  const handleLogout = () => {
+    // Lógica para cerrar sesión del usuario
+    setIsAuthenticated(false);
+  };
+  
   return (
     <header className="sticky top-0 z-50 border-b bg-white">
       <div className="mx-auto flex h-16 max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8">
         <Link to="/" className="block">
           Logo
         </Link>
-
         <div className="flex flex-1 items-center justify-end md:justify-between">
           <nav className="hidden md:block">
-            <ul className="flex ">
+            <ul className="flex">
               <li>
                 <Link to="/products">Productos</Link>
               </li>
             </ul>
           </nav>
-
           <div className="flex items-center gap-4">
             <div className="flex items-center gap-8">
               <Link to="/cart">Carrito</Link>
-              <Link to="/login">Iniciar sesión</Link>
-              <Link to="/register">Crear cuenta</Link>
+              {isAuthenticated ? (
+                <button onClick={handleLogout}>Cerrar sesión</button>
+              ) : (
+                <>
+                  <Link to="/login">Iniciar sesión</Link>
+                  <Link to="/register">Crear cuenta</Link>
+                </>
+              )}
             </div>
           </div>
         </div>
